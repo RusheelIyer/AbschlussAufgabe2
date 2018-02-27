68352b9c2896aa28c43dee03cd62df6d48ba5ed1
Rusheel <rusheel2911@gmail.com>
2018-02-27 20:31:15 +0100

    second comparator, olympic table print, new medal methods

--- IOC.java
+++ IOC.java
@@ -30,12 +30,47 @@ public class IOC {
         }  
     };
     
+    /**
+     * Create second comparator to sort the countries in the array list according to 
+     * first, the number of gold medals won, then silver, then bronze and if all three equal
+     * lastly ID
+     */
+    private static Comparator<IOC> medalComparator = new Comparator<IOC>() {
+        
+        @Override
+        public int compare(IOC countryOne, IOC countryTwo) {
+            
+            if (countryOne.goldMedals() < countryTwo.goldMedals()) {
+                return 1;
+            } else if (countryOne.goldMedals() == countryTwo.goldMedals()) {
+                if (countryOne.silverMedals() < countryTwo.silverMedals()) {
+                    return 1;
+                } else if (countryOne.silverMedals() == countryTwo.silverMedals()) {
+                    if (countryOne.bronzeMedals() < countryTwo.bronzeMedals()) {
+                        return 1;
+                    } else if (countryOne.bronzeMedals() == countryTwo.bronzeMedals()) {
+                        if (countryOne.id < countryTwo.id) {
+                            return -1;
+                        } else {
+                            return 1;
+                        }
+                    } else {
+                        return -1;
+                    }
+                } else {
+                    return -1;
+                }
+            } else { 
+                return -1;
+            }
+        }  
+    };
+    
     private short id;
     private String code;
     private String countryName;
     private int establishmentYear;
     private ArrayList<Athlete> athletes = new ArrayList<Athlete>();
-    //TODO: Add athlete to respective array when created
     
     /**
      * Constructor for an IOC-Country
@@ -117,6 +152,21 @@ public class IOC {
     }
     
     /**
+     * Method to print out the olympic medal standings with the countries
+     */
+    public static void medalTable() {
+        
+        countries.sort(medalComparator);
+        for (int i = 0; i < countries.size(); i++) {
+            Terminal.printLine("(" + i + 1 + " " + " " + String.format("%03d", countries.get(i).id)
+                    + " " + countries.get(i).code + " " + countries.get(i).countryName + " " 
+                    + countries.get(i).goldMedals() + " " + countries.get(i).silverMedals() + " "
+                    + countries.get(i).bronzeMedals() + " " + countries.get(i).calculateMedals());
+        }
+        
+    }
+    
+    /**
      * Getter for the id of the country
      * 
      * @return the id of the country
@@ -162,22 +212,59 @@ public class IOC {
     }
     
     /**
+     * Calculate the total number of gold medals won by athletes belonging to the country
+     * 
+     * @return total number of medals won
+     */
+    public int goldMedals() {
+        
+        int medalCount = 0;
+        for (int i = 0; i < this.athletes.size(); i++) {
+            medalCount = medalCount + this.athletes.get(i).getGolds();
+        }
+        
+        return medalCount;
+    }
+    
+    /**
+     * Calculate the total number of silver medals won by athletes belonging to the country
+     * 
+     * @return total number of medals won
+     */
+    public int silverMedals() {
+        
+        int medalCount = 0;
+        for (int i = 0; i < this.athletes.size(); i++) {
+            medalCount = medalCount + this.athletes.get(i).getSilvers();
+        }
+        
+        return medalCount;
+    }
+    
+    /**
+     * Calculate the total number of bronze medals won by athletes belonging to the country
+     * 
+     * @return total number of medals won
+     */
+    public int bronzeMedals() {
+        
+        int medalCount = 0;
+        for (int i = 0; i < this.athletes.size(); i++) {
+            medalCount = medalCount + this.athletes.get(i).getBronzes();
+        }
+        
+        return medalCount;
+    }
+    
+    /**
      * Calculate the total number of medals won by athletes belonging to the country
      * 
      * @return total number of medals won
      */
     public int calculateMedals() {
         
-        int medalCount = 0;
-        
-        for (int i = 0; i < this.athletes.size(); i++) {
-            int golds = this.athletes.get(i).getGolds();
-            int silvers = this.athletes.get(i).getSilvers();
-            int bronzes = this.athletes.get(i).getBronzes();
-            medalCount = medalCount + golds + silvers + bronzes;
-        }
-        
-        return medalCount;
+        return this.goldMedals() + this.silverMedals() + this.bronzeMedals();
+                
     }
 
 }
